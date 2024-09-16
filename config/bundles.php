c43fbb19b4c1a12f86c9e8d90d7cf033eddeb90c
Jason Martoux <apps+git@martoux.com>
2024-09-16 14:37:25 +0200

    add symfony-ux

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
