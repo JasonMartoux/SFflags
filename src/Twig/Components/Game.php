9df82a40a7ed5b25c9b071b3c0db69afd6fdf1ba
Jason Martoux <apps+git@martoux.com>
2024-09-16 16:07:29 +0200

    fix typo

--- src/Twig/Components/Game.php
+++ src/Twig/Components/Game.php
@@ -4,29 +4,45 @@ namespace App\Twig\Components;
 
 use Symfony\Component\Intl\Countries;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 final class Game
 {
-    use DefaultActionTrait;
+  use DefaultActionTrait;
 
-    public array $countries;
-    #[LiveProp]
-    public array $possibleFlags;
-    #[LiveProp]
-    public string $correctFlag;
+  public array $countries;
+  #[LiveProp]
+  public array $possibleFlags;
+  #[LiveProp]
+  public string $correctFlag;
 
-    public function __construct()
-    {
-        $this->countries = Countries::getNames();
-        $this->getQuestion();
+  #[LiveProp]
+  public int $score = 0;
+  #[LiveProp]
+  public int $questions = 0;
+
+  public function __construct()
+  {
+    $this->countries = Countries::getNames();
+    $this->getQuestion();
+  }
+
+  public function getQuestion(): void
+  {
+    $this->possibleFlags = array_rand($this->countries, 4);
+    $this->correctFlag = $this->possibleFlags[array_rand($this->possibleFlags)];
+  }
+
+  #[LiveAction]
+  public function check(#[LiveArg] string $flag): void
+  {
+    if ($flag === $this->correctFlag) {
+      $this->score++;
     }
-
-    public function getQuestion()
-    {
-        $this->possibleFlags = array_rand($this->countries, 4);
-        $this->correctFlag = $this->possibleFlags[array_rand($this->possibleFlags)];
-    }
+    $this->questions++;
+  }
 }
